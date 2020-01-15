673ffb2b96d1d8d467572faf0cd9434f9517cb96
Priya Ravi <priyaravi23@gmail.com>
2020-01-15 10:11:27 -0800

    lazy-loading using intersection observer

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,19 +1,40 @@
-import React, { Component } from 'react';
+import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import _ from "lodash";
 
-class HomeComponent extends Component {
-    render() {
-        return (
-            <div className="row">
-                <div className="col-sm-6 col-md-4">
-                    <img src='img/cube.gif' alt="Cube" />
-                </div>
+import { LazyImage } from "./lazy-image";
 
-                <div className="col-sm-6 col-md-4">
-                    Browse our inventory for luxury, exotic and classic cars for sale
-                </div>
-            </div>
-        );
-    }
+const Global = createGlobalStyle`
+  body {
+    background-color: #AEB6BF;
+    color: white;
+    font-family: sans-serif;
+  }
+`;
+
+const Grid = styled.section`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);;
+  row-gap: 1rem;
+  column-gap: 1rem;
+  padding: 1rem;
+  justify-items: center;
+`;
+
+export default function HomeComponent() {
+    return (
+        <main>
+            <Global />
+            <h3>Browse our inventory for luxury, exotic and classic cars for sale</h3>
+            <Grid>
+                {_.range(1000).map(index => (
+                    <LazyImage
+                        key={index}
+                        src={`https://picsum.photos/500/500/?image=${index}`}
+                        alt="placeholder"
+                    />
+                ))}
+            </Grid>
+        </main>
+    );
 }
-
-export default HomeComponent
